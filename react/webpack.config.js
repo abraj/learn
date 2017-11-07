86655cb26d7a861571f3ffac0a6b01a64fa84a9a
Abhishek Raj <abhi@raj.me>
2017-11-07 10:52:15 +0000

    ec2 commit

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,24 +1,30 @@
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
-    template: __dirname + '/app/index.html',
-    filename: 'index.html',
-    inject: 'body'
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
 });
 
 module.exports = {
-    entry: __dirname + '/app/index.js',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            }
-        ]
-    },
-    output: {
-        filename: 'transformed.js',
-        path: __dirname + '/build'
-    },
-    plugins: [HTMLWebpackPluginConfig]
+  entry: __dirname + '/app/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebpackPluginConfig],
+  devServer: {
+    host: "0.0.0.0",  // 0.0.0.0 to make the server accessible publicly
+    port: 8080,
+    //hot: true,      // default
+    //inline: true,   // default
+  }
 };
